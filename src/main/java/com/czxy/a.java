5744757d5b6aed2b2811c74f5557d7e64bf69be1
1149605808 <li114965808@outlook.com>
2019-07-27 16:11:56 +0800

    qqq

---
+++ src/main/java/com/czxy/a.java
@@ -0,0 +1,17 @@
+package com.czxy;
+
+import net.sf.jsqlparser.schema.Database;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xixilidemeilichuanshuo
+ * @version 1.0
+ * @date 2019.7.26 18:24
+ */
+@SpringBootApplication
+public class a {
+    public static void main(String[] args) {
+        SpringApplication.run(a.class,args);
+    }
+}
